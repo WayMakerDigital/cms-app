036dc83c95bcdc5cdb62a9005919aa9834c016a5
Ben <ben.newman@waymakerdigital.com>
2021-10-18 11:17:56 +0100

    Updated Base

---
+++ src/Components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Styles/NavBar.css'
+
+function NavBar() {
+    return (
+        <div className='navBar'>
+            <h1 className="navBarText">
+                CMS
+            </h1>
+        </div>
+    )
+}
+
+export default NavBar
